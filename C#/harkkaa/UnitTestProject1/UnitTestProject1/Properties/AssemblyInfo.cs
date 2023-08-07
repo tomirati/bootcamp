7e7c47768f1bfff3b31edeb4e399ead8e98602f6
Tomi Ratilainen <ratilainen.tomi@gmail.com>
2023-08-07 14:24:37 +0300

    yksikkötesti

---
+++ C#/harkkaa/UnitTestProject1/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e66b738-1a2a-4f5b-b95c-7cb1007c0655")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C#/harkkaa/yksikkötestiä/yksikkötestiä/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using yksikkötestiä;
+
+Console.WriteLine("Hello, World!");
+
+laskesumma laske = new();
+int summa = laske.Summa(1, 2);
+Console.WriteLine(summa);
